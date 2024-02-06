4057151391d492c83d91234e605a94be4bb65a99
Johnmichael Nichols <46973377+johnmichaeldnichols@users.noreply.github.com>
2024-02-05 19:27:50 -0500

    small tweaks

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d') //c = context
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024*1.3
+canvas.height = 576*1.3
 const scaledCanvas = {
     width: canvas.width / 4,
     height: canvas.height / 4
@@ -28,7 +28,6 @@ collisions.forEach((row, y) => {
     })
 })
 
-
 const platformCollisionBlocks = []
 const platformCollisions = []
 for (let i = 0; i < PLATFORM_COLLISIONS.length; i += 36){
@@ -43,22 +42,66 @@ platformCollisions.forEach((row, y) => {
                         x: x * 16,
                         y: y * 16,
                     },
+                    height: 4
                 })
             )
         }
     })
 })
 
-
-
-const gravity = 0.5
+const gravity = 0.1
 
 const player = new Player({    
     position: {
         x: 100,
-        y: 0,
+        y: 300,
     },
     collisionBlocks, // same as cB = cB
+    platformCollisionBlocks,
+    imageSrc: './img/warrior/Idle.png',
+    frameRate: 8,
+    animations: {
+        Idle: {
+            imageSrc: './img/warrior/Idle.png',
+            frameRate: 8,
+            frameBuffer: 15,
+        },
+        Run: {
+            imageSrc: './img/warrior/Run.png',
+            frameRate: 8,
+            frameBuffer: 5,
+        },
+        Jump: {
+            imageSrc: './img/warrior/Jump.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        Fall: {
+            imageSrc: './img/warrior/Fall.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        IdleLeft: {
+            imageSrc: './img/warrior/IdleLeft.png',
+            frameRate: 8,
+            frameBuffer: 15,
+        },
+        RunLeft: {
+            imageSrc: './img/warrior/RunLeft.png',
+            frameRate: 8,
+            frameBuffer: 5,
+        },
+        JumpLeft: {
+            imageSrc: './img/warrior/JumpLeft.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+        FallLeft: {
+            imageSrc: './img/warrior/FallLeft.png',
+            frameRate: 2,
+            frameBuffer: 3,
+        },
+    },
 })
 
 const background = new Sprite({
@@ -78,6 +121,15 @@ const keys = {
     },
 }
 
+const backgroundImageHeight = 432
+
+const camera = {
+    position: {
+        x: 0,
+        y: -backgroundImageHeight + scaledCanvas.height,
+    },
+}
+
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
@@ -87,23 +139,41 @@ function animate(){
     c.save()
 
     c.scale(4, 4) 
-    c.translate(0, -background.image.height + scaledCanvas.height) //translates by OG deminsions of the image
+    c.translate(camera.position.x, camera.position.y) //translates by OG deminsions of the image
     background.update()
-    collisionBlocks.forEach(collisionBlocks => {
-        collisionBlocks.update()
-    })
-    platformCollisionBlocks.forEach(collisionBlocks => {
-        collisionBlocks.update()
-    })
+    // collisionBlocks.forEach(collisionBlocks => {collisionBlocks.update()})
+    // platformCollisionBlocks.forEach(collisionBlocks => {collisionBlocks.update()})
 
+    player.checkForHorizontalCanvasCollision()
     player.update()
 
     player.velocity.x = 0
+
     if (keys.d.pressed) {
-        player.velocity.x = 3
+        player.switchSprite('Run')
+        player.velocity.x = 2
+        player.lastDirection = 'right'
+        player.panCameraLeft({canvas, camera})
     } else if (keys.a.pressed){
-        player.velocity.x = -3
+        player.switchSprite('RunLeft')
+        player.velocity.x = -2
+        player.lastDirection = 'left'
+        player.panCameraRight({canvas, camera})
+    } else if (player.velocity.y === 0) {        
+        if (player.lastDirection === 'right') player.switchSprite('Idle') 
+        else player.switchSprite('IdleLeft')        
     }
+
+    if (player.velocity.y < 0) {
+        player.panCameraDown({canvas, camera})
+        if (player.lastDirection === 'right') player.switchSprite('Jump') 
+        else player.switchSprite('JumpLeft')
+    }
+    else if (player.velocity.y > 0) {
+        player.panCameraUp({canvas, camera})
+        if (player.lastDirection === 'right') player.switchSprite('Fall') 
+        else player.switchSprite('FallLeft')
+    } 
 
     c.restore()    
 
@@ -122,8 +192,10 @@ window.addEventListener('keydown', () => {
             keys.a.pressed = true
             break
         case 'w':
-        case ' ':
-            player.velocity.y = -8
+        case ' ':                                   
+            if (player.velocity.y === 0) { player.jumpCounter = 2 }             
+            if (player.jumpCounter > 0) {player.velocity.y = -3.5}
+            player.jumpCounter--
             break
     }
 })
--- js/class/Player.js
+++ js/class/Player.js
@@ -1,41 +1,162 @@
-class Player {
-    constructor({position, collisionBlocks}) {
+class Player extends Sprite{
+    constructor({position, collisionBlocks, platformCollisionBlocks, imageSrc, frameRate, scale = 0.5, animations}) {
+        super({ imageSrc, frameRate, scale })
         this.position = position
         this.velocity = {
             x: 0,
             y: 1
         }
-        this.width = 50 / 4
-        this.height = 100 / 4
         this.collisionBlocks = collisionBlocks
+        this.platformCollisionBlocks = platformCollisionBlocks
+        this.hitbox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            width: 10,
+            height: 10,
+        }      
+
+        this.jumpCounter = 1
+
+        this.lastDirection = 'right'
+        
+        this.animations = animations  
+        for (let key in this.animations) {
+            const image = new Image()
+            image.src = this.animations[key].imageSrc            
+            this.animations[key].image = image
+        }
+
+        this.camerabox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            width: 200,
+            height: 80,
+        }
     }
 
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    switchSprite(key){
+        if (this.image === this.animations[key].image || !this.loaded) return
+
+        this.currentFrame = 0
+        this.image = this.animations[key].image
+        this.frameBuffer = this.animations[key].frameBuffer
+        this.frameRate = this.animations[key].frameRate
+    
     }
     
+    updateCamerabox() {
+        this.camerabox = {
+            position: {
+                x: this.position.x - 50 ,
+                y: this.position.y - 10 ,
+            },
+            width: 200,
+            height: 80,
+        }
+    }
+
+    panCameraLeft({canvas, camera}){
+        const cameraboxRightSide = this.camerabox.position.x + this.camerabox.width
+        const scaledCanvasWidth = canvas.width / 4
+
+        if (cameraboxRightSide >= 576) return
+
+        if (cameraboxRightSide >= scaledCanvasWidth + Math.abs(camera.position.x)) {
+            camera.position.x -= this.velocity.x
+        }
+    }
+
+    panCameraRight({canvas, camera}){    
+        if (this.camerabox.position.x <= 0) return
+        if (this.camerabox.position.x <= Math.abs(camera.position.x)){
+            camera.position.x -= this.velocity.x
+        }
+    }
+
+    panCameraDown({canvas, camera}){    
+        if (this.camerabox.position.y + this.velocity.y <= 0) return
+        if (this.camerabox.position.y <= Math.abs(camera.position.y)){
+            camera.position.y -= this.velocity.y
+        }
+    }
+
+    panCameraUp({canvas, camera}){    
+        if (this.camerabox.position.y + this.camerabox.height + this.velocity.y >= 432) return
+
+        const scaledCanvasHeight = canvas.height / 4
+        if (this.camerabox.position.y + this.camerabox.height >= Math.abs(camera.position.y) + scaledCanvasHeight){
+            camera.position.y -= this.velocity.y
+        }
+    }
+
+    checkForHorizontalCanvasCollision(){
+        if (this.hitbox.position.x + this.hitbox.width + this.velocity.x >= 576 ||
+            this.hitbox.position.x + this.velocity.x <= 0) {
+            this.velocity.x = 0
+        }        
+    }
+
     update(){
+        this.updateFrames()
+        this.updateHitbox()
+        this.updateCamerabox()
+
+        //draws out camerbox
+        // c.fillStyle = 'rgba(0, 0, 255, 0.2'
+        // c.fillRect(this.camerabox.position.x,  this.camerabox.position.y, this.camerabox.width, this.camerabox.height)
+
+        //draws out player image frame
+        // c.fillStyle = 'rgba(0, 255, 0, 0.2'
+        // c.fillRect(this.position.x,  this.position.y, this.width, this.height)
+        
+        //draws out hitbox
+        // c.fillStyle = 'rgba(255, 0, 0, 0.2'
+        // c.fillRect(this.hitbox.position.x,  this.hitbox.position.y, this.hitbox.width, this.hitbox.height)
+
         this.draw()        
+        
         this.position.x += this.velocity.x
+        this.updateHitbox()
         this.checkForHorizontalCollisions()
         this.applyGravity()
+        this.updateHitbox()
         this.checkForVerticalCollisions()
+    }
+
+    updateHitbox(){
+        this.hitbox = {
+            position: {
+                x: this.position.x + 37, //35
+                y: this.position.y + 28,  //26
+            },
+            width: 8,  //14
+            height: 24, //27
+        }     
     }
 
     checkForHorizontalCollisions() {
         for (let i = 0; i < this.collisionBlocks.length; i++){
             const collisionBlock = this.collisionBlocks[i]
-            if ( collision ({obj1: this, obj2: collisionBlock,}))
+            if ( collision ({obj1: this.hitbox, obj2: collisionBlock,}))
             {
                 if (this.velocity.x > 0) {
                     this.velocity.x = 0
-                    this.position.x = collisionBlock.position.x - this.width - 0.01
+
+                    const offset = this.hitbox.position.x - this.position.x + this.hitbox.width
+
+                    this.position.x = collisionBlock.position.x - offset - 0.01
                     break
                 }
                 if (this.velocity.x < 0) {
                     this.velocity.x = 0
-                    this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01
+
+                    const offset = this.hitbox.position.x - this.position.x
+
+                    this.position.x = collisionBlock.position.x + collisionBlock.width - offset + 0.01
                     break
                 }
             }                
@@ -43,23 +164,45 @@ class Player {
     }
 
     applyGravity() {
+        if (this.velocity.y < 2) this.velocity.y += gravity
         this.position.y += this.velocity.y        
-        this.velocity.y += gravity
     }
 
-    checkForVerticalCollisions() {
+    checkForVerticalCollisions() {        
         for (let i = 0; i < this.collisionBlocks.length; i++){
             const collisionBlock = this.collisionBlocks[i]
-            if ( collision ({obj1: this, obj2: collisionBlock,}))
+            if ( collision({obj1: this.hitbox, obj2: collisionBlock,}))
             {
                 if (this.velocity.y > 0) {
                     this.velocity.y = 0
-                    this.position.y = collisionBlock.position.y - this.height - 0.01
+
+                    const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+
+                    this.position.y = collisionBlock.position.y - offset - 0.01
                     break
                 }
                 if (this.velocity.y < 0) {
                     this.velocity.y = 0
-                    this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01
+
+                    const offset = this.hitbox.position.y - this.position.y
+
+                    this.position.y = collisionBlock.position.y + collisionBlock.height - offset + 0.01
+                    break
+                }
+            }                
+        }
+
+        //platform specific
+        for (let i = 0; i < this.platformCollisionBlocks.length; i++){
+            const platformCollisionBlock = this.platformCollisionBlocks[i]
+            if ( platformCollision({obj1: this.hitbox, obj2: platformCollisionBlock,}))
+            {
+                if (this.velocity.y > 0) {
+                    this.velocity.y = 0
+
+                    const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+
+                    this.position.y = platformCollisionBlock.position.y - offset - 0.01
                     break
                 }
             }                
